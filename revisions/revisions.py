eb88e3dd7e3371a7bcb79c556ffc265c14491c38
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2026-04-28 18:49:56 +0200

    python: add basic syntax revision(loops, conditions, functions)

---
+++ revisions/revisions.py
@@ -0,0 +1,49 @@
+# Revision
+
+# print("Hello World")  --- IGNORE ---
+
+print("Hello World")
+
+# print indentity 
+
+name = "Ludovic"
+age = 45
+print(name, age)
+
+# Conditions 
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a child.")
+
+
+# Loop
+
+for i in range(5):
+    print(i)
+
+print("")
+
+for i in range(1, 6):
+    print(i)
+
+# Functions
+
+def greet(name: str) -> str:
+    return f"Bonjour {name}!"
+
+print(greet("Ludovic"))
+
+
+# Lists
+
+my_list = [1, 2, 3]
+print(my_list)
+my_list.append(4)
+print(my_list)
+my_list.append(5)
+print(my_list)
+
+
+
