ae19d9bed9f64fb5368561651ecbccbfbc191ef4
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2026-05-06 19:34:02 +0200

    lesson_12_challenge(python): add mission 2 and 3 functions that returns boolean or multiples

--- lesson_12/lesson_12_challenge.py
+++ lesson_12/lesson_12_challenge.py
@@ -4,3 +4,47 @@ def greeting():
   return "Hello Python"
 
 print(greeting())
+
+# Mission 2: Create a function that takes in 2 numbers as parameters, and returns a boolean indicating whether or not they are equal.
+
+def are_equal(nb1, nb2):
+  if nb1 < nb2:
+    return False
+  elif nb1 > nb2:
+    return False
+  else:    return True
+
+
+print(are_equal(5, 5)) # This will return True, because 5 is equal to 5.
+print(are_equal(5, 10)) # This will return False, because 5 is not equal to 10.
+print(are_equal(10, 5)) # This will return False, because 10 is not equal to 5.
+
+# Other way to do it:
+
+def are_equal(nb1, nb2):
+  return nb1 == nb2
+
+print(are_equal(5, 5)) # This will return True, because 5 is equal to 5.
+print(are_equal(5, 10)) # This will return False, because 5 is not equal to 10.
+print(are_equal(10, 5)) # This will return False, because 10 is not equal to 5.
+
+# Mission 3 : Create a function that takes a number as a parameter, and return the first 10 multiples of the number as a list.
+
+def first_10_multiples(nb):
+  multiples = []
+  for i in range(1, 11):
+    multiples.append(i*nb)
+  
+  return multiples
+
+print(first_10_multiples(5)) # This will return [5, 10, 15, 20, 25, 30, 35, 40, 45, 50], because those are the first 10 multiples of 5.
+print(first_10_multiples(3)) # This will return [3, 6, 9, 12, 15, 18, 21, 24, 27, 30], because those are the first 10 multiples of 3.
+
+
+# Other way to do it:
+
+def first_10_multiples(nb):
+  return[i * nb for i in range(1,11)]
+
+print(first_10_multiples(5)) # This will return [5, 10, 15, 20, 25, 30, 35, 40, 45, 50], because those are the first 10 multiples of 5.
+print(first_10_multiples(3)) # This will return [3, 6, 9, 12, 15, 18, 21, 24, 27, 30], because those are the first 10 multiples of 3.
